69b2d11adfb28471a5a25877fa0c473ec678d2ba
Brent Bumann <brent.bumann@aol.com>
2019-08-30 19:38:10 -0700

    Check simple operations in swift

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,8 +2,21 @@ import UIKit
 
 var str = "Hello, playground";
 
-let item = "string \(str)"; //string interpolation
+let item: String = "string \(str)"; //string interpolation
 
 
+if(2 > 1 || 3 < 4){
+    print("this is true");
+}
 
+var list = ["apples", "bananas", "grapes"];
 
+let groceries = ["apples", "bananas", "grapes"];
+
+if(list == groceries){
+    print("These lists are the same"); //can compare lists
+}
+
+list.reverse(); //mutates arr
+
+print(list)
